fad89481defd10dc5cff10f9cc1bf6c62652ecf5
zhousl <57696015+xieyiincuit@users.noreply.github.com>
2022-04-03 16:29:16 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -9,37 +9,37 @@ const pathSrc = path.resolve(__dirname, 'src')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '~/': `${pathSrc}/`,
-      '/images': 'src/assets/images',
-      '/pic': 'src/assets/images/pic',
-      styles: path.resolve(__dirname, 'src/styles'),
-      router: path.resolve(__dirname, 'src/router'),
-      views: path.resolve(__dirname, 'src/views'),
-      components: path.resolve(__dirname, 'src/components'),
-      data: path.resolve(__dirname, 'src/data'),
-      utils: path.resolve(__dirname, 'src/utils'),
-      src: path.resolve(__dirname, 'src'),
-      assets: path.resolve(__dirname, 'src/assets'),
+    resolve: {
+        alias: {
+            '~/': `${pathSrc}/`,
+            '/images': 'src/assets/images',
+            '/pic': 'src/assets/images/pic',
+            styles: path.resolve(__dirname, 'src/styles'),
+            router: path.resolve(__dirname, 'src/router'),
+            views: path.resolve(__dirname, 'src/views'),
+            components: path.resolve(__dirname, 'src/components'),
+            data: path.resolve(__dirname, 'src/data'),
+            utils: path.resolve(__dirname, 'src/utils'),
+            src: path.resolve(__dirname, 'src'),
+            assets: path.resolve(__dirname, 'src/assets'),
+        },
     },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use "~/styles/element/index.scss" as *;`,
-      },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@use "~/styles/element/index.scss" as *;`,
+            },
+        },
     },
-  },
-  plugins: [
-    vue(),
-    Components({
-      resolvers: [
-        ElementPlusResolver({
-          importStyle: 'sass',
+    plugins: [
+        vue(),
+        Components({
+            resolvers: [
+                ElementPlusResolver({
+                    importStyle: 'sass',
+                }),
+            ],
+            dts: path.resolve(pathSrc, 'components.d.ts'),
         }),
-      ],
-      dts: path.resolve(pathSrc, 'components.d.ts'),
-    }),
-  ],
+    ],
 })
